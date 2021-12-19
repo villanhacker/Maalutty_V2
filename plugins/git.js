eec1d2f66406bd6ed81b196fb5dc43a1aa9c3efd
villanhacker <95226436+villanhacker@users.noreply.github.com>
2021-12-19 12:53:22 +0530

    😌

--- plugins/git.js
+++ plugins/git.js
@@ -17,29 +17,11 @@ Maalutty.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (mess
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `❤️Maalutty_v2💙
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `❤️chikkuz
+    
+    💙
 
-*bot making video* : https://youtu.be/jF8wF1KdyOw
-
-
-*owner number husni* :https://wa.me/917025868709.
-            
-
-*owner number Nijin* :https://wa.me/919605385305.
-
-
-*owner id instagram* :https://instagram.com/_husni_ser_.
-
-
-*github* : https://github.com/nijin-husni/Maalutty.
-
-
-*audio commands* :https://github.com/nijin-husni/uploads/tree/main/bgm.
-
-*sticker commands* :https://github.com/nijin-husni/uploads/tree/main/stickers.
-■□■□■□■□■□■□■□■□■□■□
-❤️Maalutty💙
-▣▣ ~NIJIN~ AND ~HUSNI~ ▣▣
+owner :917356145338
 
 `}) 
 
